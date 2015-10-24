c7a34f952dc87df2e2c1e7ee1495bb8610cc905f
Nick Thompson <ncthom91@gmail.com>
2015-10-24 13:53:20 -0400

    Code reuse and clean up main file

--- plot.py
+++ plot.py
@@ -4,24 +4,7 @@ import numpy as np
 from math import floor
 from wavetable.wavetable import table, gibbs_table
 from wavetable.oscillators import StandardOscillator, ResamplingOscillator, RealTimeResamplingOscillator
-
-def normalize(arr):
-    """
-    Normalize the values of the input array into the range [-1, 1].
-    """
-    arr /= np.max(np.abs(arr), axis=0)
-    return arr
-
-def trim(arr, amt):
-    """
-    Zero out the tail end of the given array.
-
-    The first N frames of the array are left in tact, where N is (1 / amt)
-    percent of the length of arr.
-    """
-    n = floor(arr.size / amt)
-    arr[n:] = 0.0
-    return arr
+from utils import normalize, trim
 
 # First, a comparison of the two wave tables. The first is our standard
 # wavetable, the second uses sigma approximation to attenuate the Gibbs
---
+++ utils.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from math import floor
+
+def normalize(arr):
+    """
+    Normalize the values of the input array into the range [-1, 1].
+    """
+    arr /= np.max(np.abs(arr), axis=0)
+    return arr
+
+def trim(arr, amt):
+    """
+    Zero out the tail end of the given array.
+
+    The first N frames of the array are left in tact, where N is (1 / amt)
+    percent of the length of arr.
+    """
+    n = floor(arr.size / amt)
+    arr[n:] = 0.0
+    return arr
